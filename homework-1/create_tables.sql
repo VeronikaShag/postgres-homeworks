3b0b22baee0d87b2a65bc48e7080665bcb67c3f5
Veronika <shagiyanovavn@gmail.ru>
2024-02-17 13:06:16 +0300

    Homework_1 create tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers_data
+(
+	customer_id varchar(50) UNIQUE,
+	company_name text,
+	contact_name varchar(200) NOT NULL
+);
+
+CREATE TABLE employees_data
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title text,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders_data
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(50) REFERENCES customers_data(customer_id) NOT NULL,
+	employee_id int REFERENCES employees_data(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(200) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,35 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+# connect to db
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='10123000v'
+)
+try:
+    with conn:
+        with conn.cursor() as cur:
+
+            with open('north_data/customers_data.csv', 'r') as f:
+                reader = csv.reader(f)
+                next(reader)  # пропуск заголовка
+                for row in reader:
+                    cur.execute("INSERT INTO customers_data VALUES (%s, %s, %s)", row)
+
+            with open('north_data/employees_data.csv', 'r') as f:
+                reader = csv.reader(f)
+                next(reader)  # пропуск заголовка
+                for row in reader:
+                    cur.execute("INSERT INTO employees_data VALUES (%s, %s, %s, %s, %s, %s)", row)
+
+            with open('north_data/orders_data.csv', 'r') as f:
+                reader = csv.reader(f)
+                next(reader)  # пропуск заголовка
+                for row in reader:
+                    cur.execute("INSERT INTO orders_data VALUES (%s, %s, %s, %s, %s)", row)
+finally:
+    conn.close()
